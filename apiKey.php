22c3a42960c25b933d7dd3c0687bc16a30e54c00
Gowtham Arputharaj <61113685+GowthamArputharaj@users.noreply.github.com>
2021-03-18 18:09:54 +0530

    Delete apiKey.php

--- apiKey.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-    $apiKey = "AAAAOOKNxp0:APA91bHDV2yawKm_S4RVydK_ofvbtu3tZFy6pBdveN6dgyuFOEReIIJ5k5Uk7EpZue7Dni2wEEY0fRq5bIK6w2fQOgW4BH2vArYTplxJB7OMYXSMQn010QshM06iCBzRk3DEgkF7PG4J";
-
-?>
